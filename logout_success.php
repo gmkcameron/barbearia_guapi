54161d5307c7a7d8c61b84311b33c4a5ad430fc7
Guilherme Cameron <gmkcameron84@gmail.com>
2024-05-18 18:37:38 -0300

    criacao logout_success.php

---
+++ logout_success.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout - Sucesso</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1>Logout realizado com sucesso!</h1>
+    <h3><a href="login.php">Fazer login novamente</a></h3>
+    <h3><a href="index.php">Ir para Home</a></>
+</body>
+</html>
