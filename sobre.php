342e874d22be09fa8d1a3d3e88cfe948d13de9aa
Guilherme Cameron <gmkcameron84@gmail.com>
2024-06-05 13:29:50 -0300

    atualizacao sobre

--- sobre.php
+++ sobre.php
@@ -1,13 +1,13 @@
 <?php include 'includes/header.php'; ?>
 
 <section>
-<p>BEM-VINDO À BARBEARIA LEONE!</p>
+<h2>Bem-vindo à Barbearia Leone!</h2>
 
 <p>Na Barbearia Leone, dedicamos nosso trabalho à arte do grooming masculino. Localizada no coração da cidade, nossa barbearia é um refúgio para homens que valorizam não apenas um bom corte de cabelo, mas também a experiência completa de cuidado e relaxamento.</p><br>
 
-<p>NOSSA HISTÓRIA<br>
+<h2>Nossa História</h2>
 
-Fundada por Leone Silva, um mestre barbeiro com mais de 20 anos de experiência, a Barbearia Leone se destaca por sua tradição e inovação. Desde a abertura, temos mantido um compromisso inabalável com a excelência e a satisfação dos nossos clientes. Aqui, cada corte é uma obra de arte, cada barba é esculpida com precisão e cada cliente é tratado como parte da família.</p>
+<p>Fundada por Leone Silva, um mestre barbeiro com mais de 20 anos de experiência, a Barbearia Leone se destaca por sua tradição e inovação. Desde a abertura, temos mantido um compromisso inabalável com a excelência e a satisfação dos nossos clientes. Aqui, cada corte é uma obra de arte, cada barba é esculpida com precisão e cada cliente é tratado como parte da família.</p>
 </section>
 
 <?php include 'includes/footer.php'; ?>
