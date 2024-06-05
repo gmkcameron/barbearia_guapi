5720561527366b255e4f8d000bf3916cb05f14c7
Guilherme Cameron <gmkcameron84@gmail.com>
2024-06-05 14:02:21 -0300

    atualizacao mapa localizacao

--- localizacao.php
+++ localizacao.php
@@ -1,8 +1,21 @@
-<?php include 'includes/header.php'; ?>
+<?php
+// Verifica se o mapa já foi incluído
+if (!isset($mapa_incluido)) {
+    $mapa_incluido = true;
+?>
+    <?php include 'includes/header.php'; ?>
 
-<section>
-    <h2>Localização</h2>
-    <p>Localizado na Rua Q, lote 18, quadra 91, Guapimirim-RJ</p>
-</section>
+    <section>
+        <h2>Localização</h2>
+        <p>Localizado na Rua Q, lote 18, quadra 91, Guapimirim-RJ</p>
+    </section>
 
-<?php include 'includes/footer.php'; ?>
+    <!-- Mapa com a localização marcada -->
+    <div style="text-align: center;">
+        <iframe width="600" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-42.99944%2C-22.5715%2C-42.99812%2C-22.57079&amp;layer=mapnik&amp;marker=-22.57114%2C-42.99878" style="border: 1px solid black"></iframe><br /><small><a href="https://www.openstreetmap.org/way/752874142#map=18/-22.57114/-42.99878">Ver mapa ampliado</a></small>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+<?php
+}
+?>
